264156e3beef9b318473e6fe160c020c0b228b26
asdaroot <129074366+asda101@users.noreply.github.com>
2024-04-17 04:19:13 +0300

    add template w gradle

--- templates/springtest/src/main/java/com/example/springtest/SpringtestApplication.java
+++ templates/springtest/src/main/java/com/example/springtest/SpringtestApplication.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springtest.other.User;
+
 @SpringBootApplication
 @RestController
 public class SpringtestApplication {
@@ -28,6 +30,8 @@ public class SpringtestApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+	  User user = new User();
+	  user.setName(name);	
+      return String.format("Hello %s!", user.getName());
     }
 }
